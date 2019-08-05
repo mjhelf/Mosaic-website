c7318583a789866693a09a2b88d16b3dd432b489
mjhelf <mjh438@cornell.edu>
2019-08-04 21:59:49 -0400

    v0.9.5 overhaul

--- files/install_METABOseek.R
+++ files/install_METABOseek.R
@@ -5,7 +5,7 @@ try({remove.packages("BiocInstaller")})
 if (!requireNamespace("BiocManager", quietly = TRUE)){
 install.packages("BiocManager")}
 
-BiocManager::install(c("xcms", "CAMERA","BiocParallel"))
+BiocManager::install(c("xcms", "CAMERA","BiocParallel"), update = TRUE, ask = FALSE)
 
 #devtools::install_github("berlinguyinca/spectra-hash", subdir="splashR")
 devtools::install_github("mjhelf/Rdisop")  ##forked from "sneumann/Rdisop"; forked version will not change and should always work. Will change once new Rdisop version is in bioc-release
@@ -14,7 +14,7 @@ devtools::install_github("mjhelf/MassTools")
 install.packages("shinyFiles")
 
 
-devtools::install_github("mjhelf/METABOseek",
+devtools::install_github("mjhelf/Metaboseek",
                          dependencies = TRUE,
                          ref = if(length(commandArgs(trailingOnly=TRUE)) > 0 ){
                            commandArgs(trailingOnly=TRUE)[1]
@@ -24,6 +24,6 @@ devtools::install_github("mjhelf/METABOseek",
 
 
 ## Start Metaboseek; this is all you need to run once you have installed Mosaic.
-library(METABOseek)
+library(Metaboseek)
 
 runMseek(launch.browser = T)
---
+++ news/build.R
@@ -0,0 +1,55 @@
+#rebuild news for all versions:
+top <- readLines("./news/top")
+news <- readLines(system.file("NEWS.html", package = "Metaboseek"))
+news <- news[grep("<body>",news)[1]:length(news)]
+
+writeLines(c(top, news),"./news/index.html")
+
+currentVer <- paste(strsplit(paste(packageVersion("Metaboseek")[[1]]),
+                             "\\.")[[1]][1:3],collapse = ".")
+
+folders <- list.dirs("./integrated")
+if(!paste0("./integrated/",currentVer) %in% folders){
+ dir.create(paste0("./integrated/",currentVer))
+    folders <- c(folders, paste0("./integrated/",currentVer))
+}
+
+lapply(folders, function(x){
+    file.copy("./news/index.html",file.path(x,"index.html"),overwrite = TRUE)
+    })
+
+#Put news on download site
+news <- readLines(system.file("NEWS.html", package = "Metaboseek"))
+newsinsert <-  news[grep('<!-- START INSERT -->',news):grep('<!-- END INSERT -->',news)]
+download <- readLines('download.html')
+new_download <- c(download[1:(grep('<!-- START INSERT -->',download)-1)],
+                  newsinsert,
+                  download[(grep('<!-- END INSERT -->',download)+1):length(download)])
+writeLines(new_download,"download.html")
+
+#update the install_Metaboseek script
+file.copy(system.file("scripts/install_Metaboseek.R", package = "Metaboseek"),
+          file.path("files/install_Metaboseek.R"), overwrite = TRUE)
+
+
+#Build documentation based on latest master version:
+download.file("https://raw.githubusercontent.com/mjhelf/METABOseek/master/vignettes/Getting_Started.Rmd",
+              "./doc/temp.Rmd")
+
+#note: images, etc have to be placed in ./doc folder manually
+library(rmarkdown)
+rmarkdown::render(file.path(getwd(),"doc/temp.Rmd"),
+       output_format = "html_document", output_file = "Getting_Started.html",
+       output_dir = NULL,
+       output_options = list(
+           css = "style.css",
+           theme = "simplex",
+           toc = TRUE,
+           number_sections = TRUE,
+           toc_depth = 2,
+           toc_float = list(collapsed = FALSE, smooth_scroll = TRUE)),
+       intermediates_dir = NULL,
+       knit_root_dir = NULL, runtime = c("auto", "static", "shiny",
+                                         "shiny_prerendered"), clean = TRUE, params = NULL,
+       knit_meta = NULL, envir = parent.frame(), run_pandoc = TRUE,
+       quiet = FALSE, encoding = "UTF-8")
